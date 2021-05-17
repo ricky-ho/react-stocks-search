ea2779e5ecd4af656c3db42c06c93b7feff715d4
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-05-16 23:12:15 -0700

    Added moment.js
    Use moment.js to calculate relative time formats

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export const unixToLocaleDate = (unix_datetime) => {
   if (unix_datetime === null) return null;
 
@@ -13,20 +15,6 @@ export const unixToLocaleDate = (unix_datetime) => {
   });
 };
 
-export const getRelativeTime = (d1, d2 = new Date()) => {
-  const units = {
-    year: 24 * 60 * 60 * 1000 * 365,
-    month: (24 * 60 * 60 * 1000 * 365) / 12,
-    day: 24 * 60 * 60 * 1000,
-    hour: 60 * 60 * 1000,
-    minute: 60 * 1000,
-    second: 1000,
-  };
-  const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
-  const elapsed = d1 - d2;
-
-  for (let u in units) {
-    if (Math.abs(elapsed) > units[u] || u == "second")
-      return rtf.format(Math.round(elapsed / units[u]), u);
-  }
+export const getRelativeTime = (timestamp) => {
+  return moment(timestamp).fromNow();
 };
